19c79161530f6d15a83e0a8f647c7b7fc96ee004
Edgar Alexander Aguilar <ea.aguilar@uniandes.edu.co>
2017-08-01 14:03:45 -0500

    Enlace creacion solicitantes

---
+++ PruebaMvc/Controllers/AdministrarOferenteController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaMvc.Controllers
+{
+    public class AdministrarOferenteController : Controller
+    {
+        // GET: AdmnistrarOferente
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+
+
+
+    }
+}
